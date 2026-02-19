3ffc282d9e7774040c003e09625805abd5b22310
miniignis <nieto.aidan.1@gmail.com>
2026-02-19 11:21:17 -0600

    config created

---
+++ main.lua
@@ -0,0 +1,19 @@
+function love.config(t)
+    t.title = "Particle Toy"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = false
+    t.window.vsync = 1
+end
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    
+end
